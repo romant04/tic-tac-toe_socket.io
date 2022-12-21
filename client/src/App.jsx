4ad20578cfee0003856329e5721f28f8a131ca02
Roman Tarnai <sirdiogo@seznam.cz>
2022-12-21 10:31:44 +0100

    idk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import io from 'socket.io-client'
 import './styles/main.css'
 
 function App() {
-  const socket = io('http://localhost:5000')
+  const socket = io('https://tarnai-piskvorky.herokuapp.com/')
 
   return (
     <Router>
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const server = http.createServer(app)
 
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
+
 app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.get('*', (req, res) => {
@@ -82,8 +84,6 @@ io.on('connection', (socket) => {
     }
   })
 })
-
-app.use(cors())
 
 server.listen(PORT, () => {
   console.log(`Listening to ${PORT}`)
