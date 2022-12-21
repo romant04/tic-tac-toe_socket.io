4fb6d7cafb6772d380e88650ebb39ac2b39a1d53
Roman Tarnai <sirdiogo@seznam.cz>
2022-12-21 11:35:13 +0100

    gameLogic fix

--- client/src/utils/gameLogic.js
+++ client/src/utils/gameLogic.js
@@ -1,39 +1,47 @@
 export function isValid(target) {
-    if (target === "") {
-        return true;
-    } else {
-        return false;
-    }
+  if (target === '') {
+    return true
+  } else {
+    return false
+  }
 }
 
 export function isWinner(board, player) {
-    // Check rows
-    if (board[0] === player && board[1] === player && board[2] === player) return true;
-    if (board[3] === player && board[4] === player && board[5] === player) return true;
-    if (board[6] === player && board[7] === player && board[8] === player) return true;
+  // Check rows
+  if (board[0] === player && board[1] === player && board[2] === player)
+    return true
+  if (board[3] === player && board[4] === player && board[5] === player)
+    return true
+  if (board[6] === player && board[7] === player && board[8] === player)
+    return true
 
-    // Check columns
-    if (board[0] === player && board[3] === player && board[6] === player) return true;
-    if (board[1] === player && board[4] === player && board[7] === player) return true;
-    if (board[2] === player && board[5] === player && board[8] === player) return true;
+  // Check columns
+  if (board[0] === player && board[3] === player && board[6] === player)
+    return true
+  if (board[1] === player && board[4] === player && board[7] === player)
+    return true
+  if (board[2] === player && board[5] === player && board[8] === player)
+    return true
 
-    // Check diagonals
-    if (board[0] === player && board[4] === player && board[8] === player) return true;
-    if (board[2] === player && board[4] === player && board[6] === player) return true;
+  // Check diagonals
+  if (board[0] === player && board[4] === player && board[8] === player)
+    return true
+  if (board[2] === player && board[4] === player && board[6] === player)
+    return true
+  // Check draw
+  else if (
+    board[0] !== '' &&
+    board[1] !== '' &&
+    board[2] !== '' &&
+    board[3] !== '' &&
+    board[4] !== '' &&
+    board[5] !== '' &&
+    board[6] !== '' &&
+    board[7] !== '' &&
+    board[8] !== '' &&
+    !isWinner(player === 'X' ? 'O' : 'X')
+  )
+    return 'draw'
 
-    // Check draw
-    if (
-        board[0] !== "" &&
-        board[1] !== "" &&
-        board[2] !== "" &&
-        board[3] !== "" &&
-        board[4] !== "" &&
-        board[5] !== "" &&
-        board[6] !== "" &&
-        board[7] !== "" &&
-        board[8] !== ""
-    )
-        return "draw";
-
-    return false;
+  return false
 }
